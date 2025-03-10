d49ad3bb2f6a10beeab0962bfbc69d7347220347
Shiming Zhang <wzshiming@hotmail.com>
2025-03-10 12:03:12 +0800

    Reuse objects to avoid GC

--- cmd/hosts-forward/main.go
+++ cmd/hosts-forward/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"log/slog"
@@ -36,21 +35,20 @@ func init() {
 }
 
 func main() {
+	// TODO: Supports graceful shutdown
 	ctx := context.Background()
 
+	go startRedirector()
+	startListener(ctx)
+}
+
+func startListener(ctx context.Context) {
 	listener, err := net.Listen("tcp", ":443")
 	if err != nil {
 		slog.Error("Failed to create listener", "err", err, "address", listener.Addr().String())
 		os.Exit(1)
 	}
 
-	go startRedirector()
-	go startListener(ctx, listener)
-
-	select {}
-}
-
-func startListener(ctx context.Context, listener net.Listener) {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -81,13 +79,23 @@ func redirectToHTTPS(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, r.URL.String(), http.StatusFound)
 }
 
-func handleConnection(ctx context.Context, conn net.Conn, from string) {
-	defer conn.Close()
+var bufferPool = sync.Pool{
+	New: func() any {
+		return bytes.NewBuffer(nil)
+	},
+}
 
-	buf := bytes.NewBuffer(nil)
+func handleConnection(ctx context.Context, conn net.Conn, from string) {
+	buf := bufferPool.Get().(*bytes.Buffer)
+	defer func() {
+		buf.Reset()
+		bufferPool.Put(buf)
+	}()
+
 	host, err := sni.TLSHost(io.TeeReader(conn, buf))
 	if err != nil {
 		if sniFallbackEndpoint == "" {
+			conn.Close()
 			slog.Warn("SNI parse failure", "from", from, "err", err)
 			return
 		}
@@ -96,16 +104,17 @@ func handleConnection(ctx context.Context, conn net.Conn, from string) {
 
 	targetAddr, err := determineTargetAddress(host, from)
 	if err != nil {
+		conn.Close()
 		slog.Warn("Target resolution failed", "host", host, "from", from, "err", err)
 		return
 	}
 
 	forwardConn, err := net.Dial("tcp", net.JoinHostPort(targetAddr, "443"))
 	if err != nil {
+		conn.Close()
 		slog.Warn("Target connection failed", "target", targetAddr, "host", host, "from", from, "err", err)
 		return
 	}
-	defer forwardConn.Close()
 
 	slog.Info("Starting proxy tunnel", "target", targetAddr, "host", host, "from", from)
 	if err := tunnel(ctx, cmux.UnreadConn(conn, buf.Bytes()), forwardConn); err != nil {
@@ -141,31 +150,59 @@ func determineTargetAddress(host, from string) (string, error) {
 	return resolvedAddr, nil
 }
 
-func tunnel(ctx context.Context, c1, c2 io.ReadWriter) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+var bytesPool = sync.Pool{
+	New: func() any {
+		return make([]byte, 32*1024)
+	},
+}
 
-	var errs [3]error
+var errChPool = sync.Pool{
+	New: func() any {
+		return make(chan error, 2)
+	},
+}
 
-	go func() {
-		_, errs[0] = io.Copy(c1, c2)
-		cancel()
-	}()
-
-	go func() {
-		_, errs[1] = io.Copy(c2, c1)
-		cancel()
-	}()
-
-	<-ctx.Done()
-
-	errs[2] = ctx.Err()
-
-	if errs[2] == context.Canceled {
-		errs[2] = nil
+func drainCh(ch chan error) {
+	for {
+		select {
+		case <-ch:
+		default:
+			return
+		}
 	}
+}
 
-	return errors.Join(errs[:]...)
+func tunnel(ctx context.Context, c1, c2 io.ReadWriteCloser) error {
+	buf1 := bytesPool.Get().([]byte)
+	buf2 := bytesPool.Get().([]byte)
+	errCh := errChPool.Get().(chan error)
+	defer func() {
+		c1.Close()
+		c2.Close()
+
+		bytesPool.Put(buf1)
+		bytesPool.Put(buf2)
+
+		drainCh(errCh)
+		errChPool.Put(errCh)
+	}()
+
+	go func() {
+		_, err := io.CopyBuffer(c1, c2, buf1)
+		errCh <- err
+	}()
+
+	go func() {
+		_, err := io.CopyBuffer(c2, c1, buf2)
+		errCh <- err
+	}()
+
+	select {
+	case err := <-errCh:
+		return err
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 }
 
 var ipCache sync.Map
